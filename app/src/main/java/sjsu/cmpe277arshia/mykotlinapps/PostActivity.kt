3a86808f3a8bed6c096651d2264cee865e37a280
Arshia Sali <arshiasali@gmail.com>
2021-03-12 12:55:59 -0800

    Added another activity(view) file

---
+++ app/src/main/java/sjsu/cmpe277arshia/mykotlinapps/PostActivity.kt
@@ -0,0 +1,11 @@
+package sjsu.cmpe277arshia.mykotlinapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post)
+    }
+}
